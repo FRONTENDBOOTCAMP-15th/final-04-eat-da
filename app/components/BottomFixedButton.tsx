3834c02477b891d26200ad58a83af8e1b97d60d7
UJin1122 <enooooo93@gmail.com>
2026-01-21 13:38:36 +0900

    design: 버튼 컴포넌트 수정

--- app/components/BottomFixedButton.tsx
+++ app/components/BottomFixedButton.tsx
@@ -14,7 +14,7 @@ type BottomFixedButtonProps =
 
 export default function BottomFixedButton(props: BottomFixedButtonProps) {
   const className =
-    "fixed bottom-0 w-full h-22.5 flex items-center justify-center text-white font-semibold bg-eatda-orange hover:opacity-90";
+    "fixed bottom-0 w-full h-25 flex justify-center pt-6.75 pb-15 text-white font-semibold bg-eatda-orange hover:opacity-90";
 
   if (props.as === "link") {
     return (
