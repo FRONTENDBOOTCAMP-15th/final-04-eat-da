024ced650be84813fd7932105090c7ce82ef70bd
UJin1122 <enooooo93@gmail.com>
2026-01-21 22:50:19 +0900

    design: 마이페이지 수정

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -7,5 +7,14 @@ export const metadata: Metadata = {
 };
 
 export default function MyPage() {
-  return <MyPageClient />;
+  return (
+    <div className="px-5 flex flex-col gap-5">
+      <header className="flex gap-4 pt-21">
+        <h1 className="text-display-6 text-gray-800 font-semibold">
+          마이페이지
+        </h1>
+      </header>
+      <MyPageClient />
+    </div>
+  );
 }
