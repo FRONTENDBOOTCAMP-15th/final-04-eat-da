69ebdf14717aebbbb6e7748fd0a912256f8ee420
jiyou228 <jhjy504@naver.com>
2026-01-22 11:17:18 +0900

    design: home 마크업 완료

--- app/mypage/banchanmanagement/page.tsx
+++ app/mypage/banchanmanagement/page.tsx
@@ -1,4 +1,4 @@
-import BottomFixedButton from "@/app/components/BottomFixedButton";
+import BottomFixedButton from "@/app/src/components/common/BottomFixedButton";
 import BanchanManagementClient from "@/app/mypage/banchanmanagement/BanchanManagementClient";
 import { Metadata } from "next";
 import Link from "next/link";
@@ -12,16 +12,29 @@ export default function BanchanManagement() {
   return (
     <div className="flex flex-col min-h-screen">
       <div className="px-5 flex flex-col gap-7.5 flex-1 pb-22.5">
-        
         <header className="flex gap-4 pt-21 ">
           <Link href={"/mypage"} className="flex items-center">
             <span>
-              <svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M9 0.999999L1 9L9 17" stroke="#353E5C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              <svg
+                width="10"
+                height="18"
+                viewBox="0 0 10 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M9 0.999999L1 9L9 17"
+                  stroke="#353E5C"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
               </svg>
             </span>
           </Link>
-          <h1 className="text-display-6 text-gray-800 font-semibold">반찬 관리</h1>
+          <h1 className="text-display-6 text-gray-800 font-semibold">
+            반찬 관리
+          </h1>
         </header>
         <BanchanManagementClient />
       </div>
