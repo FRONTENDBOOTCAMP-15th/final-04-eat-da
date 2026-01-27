86acd95052ee818f7a018d877cb34ad5ecabcdc7
UJin1122 <enooooo93@gmail.com>
2026-01-27 11:35:51 +0900

    design: 모달 버튼 글자 크기/패딩값/rounded수정

--- app/src/components/ui/ConfirmModal.tsx
+++ app/src/components/ui/ConfirmModal.tsx
@@ -17,9 +17,9 @@ export default function ConfirmModal({
 
   return (
     <div className="fixed inset-0 bg-black/70 flex items-center justify-center z-50 p-5">
-      <div className="bg-white rounded w-[90vw] max-w-150 p-6 flex flex-col gap-6.5">
-        <div className="text-gray-800 text-center flex flex-col gap-4">
-          <p className="text-display-5 font-semibold">{title}</p>
+      <div className="bg-white rounded-lg w-[90vw] max-w-150 px-6 pb-4 pt-8 flex flex-col gap-4">
+        <div className="text-gray-800 text-center flex flex-col gap-1">
+          <p className="text-display-4 font-semibold">{title}</p>
           {description && <p className="text-display-2">{description}</p>}
         </div>
 
