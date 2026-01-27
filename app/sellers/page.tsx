eadc0b48be17a2ef9760e5f0d84a775c5b4f907d
zinapark <pje903@gmail.com>
2026-01-27 13:10:36 +0900

    design: 주부 리스트 map방식으로 변경

--- app/sellers/page.tsx
+++ app/sellers/page.tsx
@@ -3,6 +3,15 @@ import BottomFixedButton from "@/app/src/components/common/BottomFixedButton";
 import BottomNavigation from "@/app/src/components/common/BottomNavigation";
 import Header from "@/app/src/components/common/Header";
 
+const sellers = [
+  { id: "s1" },
+  { id: "s2" },
+  { id: "s3" },
+  { id: "s4" },
+  { id: "s5" },
+  { id: "s6" },
+];
+
 export default function SellersList() {
   {
     /* 리뷰 리스트 */
@@ -13,13 +22,13 @@ export default function SellersList() {
     <div className="flex flex-col gap-7.5 mt-15 pb-23">
       <Header title="주부 목록" showBackButton showSearch showCart />
       <div>
-        <SellerCard />
-        <SellerCard />
-        <SellerCard />
-        <SellerCard />
-        <SellerCard />
-        <SellerCard />
+        {sellers.map((seller, index) => {
+          const isLast = index === sellers.length - 1;
+
+          return <SellerCard key={seller.id} showDivider={!isLast} />;
+        })}
       </div>
+
       <BottomNavigation />
     </div>
   );
