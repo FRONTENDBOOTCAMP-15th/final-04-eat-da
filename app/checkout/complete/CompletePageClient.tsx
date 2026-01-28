2512bb56817a51e1f3ceffeaabfffbb148b343c2
jiyou228 <jhjy504@naver.com>
2026-01-28 16:22:34 +0900

    refeactor: types 코드 파일 분리하기

--- app/checkout/complete/CompletePageClient.tsx
+++ app/checkout/complete/CompletePageClient.tsx
@@ -7,46 +7,9 @@ import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import dayjs from "dayjs";
 import "dayjs/locale/ko";
+import { OrderData } from "@/app/src/types";
 
 dayjs.locale("ko");
-
-interface OrderProduct {
-  _id: number;
-  quantity: number;
-  seller_id: number;
-  name: string;
-  seller: {
-    name: string;
-    _id: number;
-  };
-  image: {
-    path: string;
-    name: string;
-  };
-  price: number;
-  extra?: any;
-}
-
-interface OrderData {
-  _id: number;
-  products: OrderProduct[];
-  extra: {
-    pickupDate: string;
-    pickupTime: string;
-    pickupPlace: string;
-  };
-  cost: {
-    products: number;
-    shippingFees: number;
-    discount: {
-      products: number;
-      shippingFees: number;
-    };
-    total: number;
-  };
-  createdAt: string;
-  updatedAt: string;
-}
 
 export default function CompletePageClient() {
   const searchParams = useSearchParams();
