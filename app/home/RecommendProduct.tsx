b72bd60d0764672c7ef87353702203162925658e
jiyou228 <jhjy504@naver.com>
2026-02-02 14:28:52 +0900

    fix: 추천 반찬 사이즈 속성 부여

--- app/home/RecommendProduct.tsx
+++ app/home/RecommendProduct.tsx
@@ -14,6 +14,7 @@ export default function RecommendProduct({ product }: RecommendProductProps) {
         <Image
           src={product.mainImages?.[0]?.path || '/food1.png'}
           fill
+          sizes="(max-width: 768px) 50vw, 300px"
           alt={product.name}
           className="object-cover rounded-lg"
         />
