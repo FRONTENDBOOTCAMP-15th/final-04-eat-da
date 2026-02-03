709da3c22637a0d047fb8d6301eb756683668960
zinapark <pje903@gmail.com>
2026-02-03 14:28:22 +0900

    fix: 이미지 경로 페이지 주소 수정

--- next.config.ts
+++ next.config.ts
@@ -1,43 +1,22 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
+      { protocol: 'https', hostname: 'picsum.photos', pathname: '/**' },
+      { protocol: 'https', hostname: 'res.cloudinary.com', pathname: '/**' },
       {
-        protocol: "https",
-        hostname: "picsum.photos",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'zipbanchan.godohosting.com',
+        pathname: '/**',
       },
+      { protocol: 'https', hostname: 'cdn-ddanzi.bizhost.kr', pathname: '/**' },
       {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'thingool123.godohosting.com',
+        pathname: '/**',
       },
-      {
-        protocol: "https",
-        hostname: "zipbanchan.godohosting.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn-ddanzi.bizhost.kr",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "thingool123.godohosting.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "zipbanchan.godohosting.com",
-      },
-      {
-        protocol: "https",
-        hostname: "api.dicebear.com",
-        port: "",
-        pathname: "/**",
-      },
+      { protocol: 'https', hostname: 'api.dicebear.com', pathname: '/**' },
     ],
   },
 };
