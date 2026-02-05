975c3f2a67634328c947d187447391d88b1278cb
zinapark <pje903@gmail.com>
2026-02-05 17:08:36 +0900

    feat: 바텀버튼 비활성화 상태 추가

--- app/src/components/common/BottomFixedButton.tsx
+++ app/src/components/common/BottomFixedButton.tsx
@@ -1,29 +1,30 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 type BottomFixedButtonProps =
   | {
-      as: "link";
+      as: 'link';
       href: string;
       children: React.ReactNode;
     }
   | {
-      as: "button";
-      type?: "submit";
+      as: 'button';
+      type?: 'submit';
       formId: string;
       children: React.ReactNode;
     }
   | {
-      as: "button";
-      type: "button";
+      as: 'button';
+      type: 'button';
       onClick: () => void | Promise<void>;
       children: React.ReactNode;
+      disabled?: boolean;
     };
 
 export default function BottomFixedButton(props: BottomFixedButtonProps) {
   const className =
-    "fixed bottom-0 w-full h-17 flex items-center justify-center pt-2 pb-2 text-white text-display-4 font-semibold bg-eatda-orange hover:opacity-90";
+    'fixed bottom-0 w-full h-17 flex items-center justify-center pt-2 pb-2 text-white text-display-4 font-semibold bg-eatda-orange hover:opacity-90';
 
-  if (props.as === "link") {
+  if (props.as === 'link') {
     return (
       <Link href={props.href} className={className}>
         {props.children}
@@ -32,9 +33,17 @@ export default function BottomFixedButton(props: BottomFixedButtonProps) {
   }
 
   // 일반 클릭 버튼
-  if (props.type === "button") {
+  if (props.type === 'button') {
+    const buttonClassName = props.disabled
+      ? 'fixed bottom-0 w-full h-17 flex items-center justify-center pt-2 pb-2 text-gray-500 text-display-4 font-semibold bg-gray-300 cursor-not-allowed'
+      : className;
     return (
-      <button type="button" onClick={props.onClick} className={className}>
+      <button
+        type="button"
+        onClick={props.disabled ? undefined : props.onClick}
+        className={buttonClassName}
+        disabled={props.disabled}
+      >
         {props.children}
       </button>
     );
