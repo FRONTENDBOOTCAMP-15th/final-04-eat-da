b62d9bc1b26f6c9f6c2409efea5afeec1822c105
jiyou228 <jhjy504@naver.com>
2026-02-11 13:37:31 +0900

    design: open graph 이미지 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,20 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: '잇다 - 우리 동네 주부님의 집밥',
   description: '정성 가득 집밥 한 끼',
+  openGraph: {
+    title: '잇다 - 우리 동네 주부님의 집밥',
+    description: '정성 가득 집밥 한 끼',
+    images: [
+      {
+        url: 'https://res.cloudinary.com/ddedslqvv/image/upload/v1770784591/febc15-final04-ecad/RJy6JDRqV.jpg',
+        width: 1200,
+        height: 630,
+        alt: '잇다 대표 이미지',
+      },
+    ],
+    type: 'website',
+    siteName: '잇다',
+  },
 };
 
 export default function RootLayout({
