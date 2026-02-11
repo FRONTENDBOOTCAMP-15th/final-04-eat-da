75e6ca22a0c26d061d4db9bda941a1830087b616
jiyou228 <jhjy504@naver.com>
2026-02-11 13:08:47 +0900

    feat: 기본 메타 데이터 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,18 @@
 import './globals.css';
 import './src/styles/tailwind.css';
 import { Inter } from 'next/font/google';
+import { Metadata } from 'next';
 
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
   display: 'swap',
 });
+
+export const metadata: Metadata = {
+  title: '잇다 - 우리 동네 주부님의 집밥',
+  description: '정성 가득 집밥 한 끼',
+};
 
 export default function RootLayout({
   children,
